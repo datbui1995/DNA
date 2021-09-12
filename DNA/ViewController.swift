9a09b3d3501b56c00864be12b2a94db17cf63213
Dat Bui <dat.buitat@ascendcorp.com>
2021-09-12 18:13:28 +0700

    Initial Commit

---
+++ DNA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DNA
+//
+//  Created by Dat Bui on 12/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
