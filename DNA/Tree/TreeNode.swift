e90a02cbc7fe2ab1ffdacab0be94a9bb886b0e0e
Dat Bui <datbui2307@gmail.com>
2021-12-21 01:58:19 +0700

    binary tree traversal level order

--- DNA/Tree/TreeNode.swift
+++ DNA/Tree/TreeNode.swift
@@ -78,4 +78,35 @@ class Solution {
         values.append(root!.val)
         return values
     }
+    
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        guard let root = root else { return [] }
+        var queue = [TreeNode]()
+        var output = [[Int]]()
+        queue.append(root)
+        
+        while queue.isEmpty == false {
+            // start new level
+            // number of elements in the current level
+            var levelCount = queue.count
+            var levelNodes = [Int]()
+            while levelCount > 0 {
+                let node = queue.first!
+                queue.removeFirst()
+                levelCount -= 1
+                levelNodes.append(node.val)
+                // Add Left child node
+                if let left = node.left{
+                    queue.append(left)
+                }
+                // Add right child node
+                if let right = node.right{
+                    queue.append(right)
+                }
+            }
+            // Add nodes by level
+            output.append(levelNodes)
+        }
+        return output
+    }
 }
