7dfd28fe971f55745f4d792ae107b39699a11aa8
Dat Bui <datbui2307@gmail.com>
2022-07-08 04:19:03 +0700

    Leetcode resolving

--- DNA/Leetcode/Solution+Array.swift
+++ DNA/Leetcode/Solution+Array.swift
@@ -11,24 +11,24 @@ class Solution {
     
     /*
      Given an array of integers arr, return true if and only if it is a valid mountain array.
-
+     
      Recall that arr is a mountain array if and only if:
-
+     
      arr.length >= 3
      There exists some i with 0 < i < arr.length - 1 such that:
      arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
      arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
      
      Example 1:
-
+     
      Input: arr = [2,1]
      Output: false
      Example 2:
-
+     
      Input: arr = [3,5,5]
      Output: false
      Example 3:
-
+     
      Input: arr = [0,3,2,1]
      Output: true
      */
@@ -62,7 +62,7 @@ class Solution {
     
     /*
      Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
-
+     
      You must write an algorithm with O(log n) runtime complexity.
      
      Input: nums = [1,3,5,6], target = 5
@@ -92,5 +92,37 @@ class Solution {
                 }
             }
         }
+    }
+    
+    /*
+     You are given an array of people, people, which are the attributes of some people in a queue (not necessarily in order). Each people[i] = [hi, ki] represents the ith person of height hi with exactly ki other people in front who have a height greater than or equal to hi.
+     
+     Reconstruct and return the queue that is represented by the input array people. The returned queue should be formatted as an array queue, where queue[j] = [hj, kj] is the attributes of the jth person in the queue (queue[0] is the person at the front of the queue).
+     
+     
+     
+     Example 1:
+     
+     Input: people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+     Output: [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
+     Explanation:
+     Person 0 has height 5 with no other people taller or the same height in front.
+     Person 1 has height 7 with no other people taller or the same height in front.
+     Person 2 has height 5 with two persons taller or the same height in front, which is person 0 and 1.
+     Person 3 has height 6 with one person taller or the same height in front, which is person 1.
+     Person 4 has height 4 with four people taller or the same height in front, which are people 0, 1, 2, and 3.
+     Person 5 has height 7 with one person taller or the same height in front, which is person 1.
+     Hence [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]] is the reconstructed queue.
+     Example 2:
+     
+     Input: people = [[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]]
+     Output: [[4,0],[5,0],[2,2],[3,2],[1,4],[6,0]]
+     */
+    
+    func reconstructQueue(_ people: [[Int]]) -> [[Int]] {
+        var result: [[Int]] = []
+        people.sorted(by: { ($0[0] == $1[0]) ? $0[1] < $1[1] : $0[0] > $1[0] })
+            .forEach({result.insert($0, at: $0[1])})
+        return result
     }
 }
