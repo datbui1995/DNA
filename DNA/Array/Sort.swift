063a1619c47fc20f978b94a54a96964b9dad1c0f
Dat Bui <32448958+datbui1995@users.noreply.github.com>
2025-12-18 15:01:08 +0700

    implement Bu

--- DNA/Array/Sort.swift
+++ DNA/Array/Sort.swift
@@ -42,6 +42,7 @@ class CustomSort {
         return final
     }
     
+    /// Selection Sort
     func selectionSort(arr: [Int]) -> [Int] {
         var minIndex = 0
         var arr = arr
@@ -55,6 +56,21 @@ class CustomSort {
             let temp = arr[minIndex]
             arr[minIndex] = arr[i]
             arr[i] = temp
+        }
+        return arr
+    }
+    
+    /// Bubble Sort
+    func bubbleSort(arr: [Int]) -> [Int] {
+        var arr = arr
+        for i in 0..<arr.count {
+            for j in 0..<(arr.count-i-1) {
+                if arr[j] > arr[j+1] {
+                    let temp = arr[j]
+                    arr[j] = arr[j+1]
+                    arr[j+1] = temp
+                }
+            }
         }
         return arr
     }
